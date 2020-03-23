e026dcbd792df95a578556a469c67f5d82578da1
mathieu <matjuillard@hotmail.com>
2020-03-23 17:42:49 +0100

    adding Forms - Template driven for shoping list item vs reactive for recipe item

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+
+  // Event triggered when recipes list changed
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
       'A first recipe desc',
-      'https://xxxxx.jpg',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/easy-chicken-fajitas.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French fries', 20)
@@ -19,7 +23,7 @@ export class RecipeService {
     new Recipe(
       'Another test recipe',
       'desc of a birger recipe',
-      'https://xxxxx.jpg',
+      'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
@@ -39,5 +43,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,7 +4,8 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
 
-ingredientsChanged = new Subject<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('apple', 5),
@@ -15,6 +16,10 @@ ingredientsChanged = new Subject<Ingredient[]>();
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   addIngredient( ingredient: Ingredient ) {
     this.ingredients.push( ingredient );
     this.ingredientsChanged.next(this.ingredients.slice());
@@ -22,6 +27,16 @@ ingredientsChanged = new Subject<Ingredient[]>();
 
   addIngredients( ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
